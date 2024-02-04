631d48e63d96cabe9e781fcfee9c6d8f691c4c72
Emanuel Jimenez <emanuel.jimenez9123@alumnos.udg.mx>
2024-02-04 11:29:05 -0600

    Modal para ver las receteas

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -7,20 +7,39 @@ import {
   DialogPanel,
   DialogTitle,
 } from '@headlessui/vue'
+import { useModalStore } from '../stores/modal'
+import { useBebidasStore } from '../stores/bebidas'
 
-const isOpen = ref(true)
+const modal = useModalStore()
+const bebidas = useBebidasStore()
 
-function closeModal() {
-  isOpen.value = false
-}
-function openModal() {
-  isOpen.value = true
+const formatearIngredientes = () => {
+  const ingredientesDiv = document.createElement('DIV')
+
+  for (let i = 1; i <= 15; i++) {
+    if (bebidas.receta[`strIngredient${i}`]) {
+      const ingrediente = bebidas.receta[`strIngredient${i}`]
+      const cantidad = bebidas.receta[`strMeasure${i}`]
+
+      const ingredienteCantidad = document.createElement('P')
+      ingredienteCantidad.classList.add('text-md', 'text-gray-500')
+      ingredienteCantidad.textContent = `${ingrediente} - ${cantidad}`
+
+      ingredientesDiv.appendChild(ingredienteCantidad)
+    }
+  }
+
+  return ingredientesDiv
 }
 </script>
 
 <template>
-  <TransitionRoot appear :show="isOpen" as="template">
-    <Dialog as="div" @close="closeModal" class="relative z-10">
+  <TransitionRoot 
+    appear 
+    :show="modal.modal" 
+    as="template">
+
+    <Dialog as="div" @close="modal.handleClickModal()" class="relative z-10">
       <TransitionChild
         as="template"
         enter="duration-300 ease-out"
@@ -30,7 +49,7 @@ function openModal() {
         leave-from="opacity-100"
         leave-to="opacity-0"
       >
-        <div class="fixed inset-0 bg-black/25" />
+        <div class="fixed inset-0 bg-black/50" />
       </TransitionChild>
 
       <div class="fixed inset-0 overflow-y-auto">
@@ -47,28 +66,52 @@ function openModal() {
             leave-to="opacity-0 scale-95"
           >
             <DialogPanel
-              class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
+              class="w-full max-w-xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
             >
-              <DialogTitle
-                as="h3"
-                class="text-lg font-medium leading-6 text-gray-900"
-              >
-                Payment successful
-              </DialogTitle>
-              <div class="mt-2">
-                <p class="text-sm text-gray-500">
-                  Your payment has been successfully submitted. We’ve sent you
-                  an email with all of the details of your order.
-                </p>
+              <div class="flex flex-col gap-4">
+                <DialogTitle
+                  as="h3"
+                  class="text-3xl font-extrabold leading-6 text-gray-900"
+                >
+                  {{ bebidas.receta.strDrink }}
+                </DialogTitle>
+                
+                <img 
+                  :src="bebidas.receta.strDrinkThumb" 
+                  :alt="'Imagen de ' + bebidas.receta.strDrink"
+                  class="mx-auto w-96 my-4"
+                />
+              </div>
+              
+              <div class="mt-4 flex flex-col gap-2">
+                <DialogTitle
+                  as="h3"
+                  class="text-2xl font-extrabold leading-6 text-gray-900"
+                >
+                  Ingredientes y Cantidades
+                </DialogTitle>
+
+                <div v-html="formatearIngredientes().outerHTML"></div>
+              </div>
+
+              <div class="mt-4 flex flex-col gap-2">
+                <DialogTitle
+                  as="h3"
+                  class="text-2xl font-extrabold leading-6 text-gray-900"
+                >
+                  Instrucciones
+                </DialogTitle>
+
+                <p class="text-md text-gray-500">{{ bebidas.receta.strInstructions }}</p>
               </div>
 
               <div class="mt-4">
                 <button
                   type="button"
-                  class="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                  @click="closeModal"
+                  class="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 mt-4"
+                  @click="modal.handleClickModal()"
                 >
-                  Got it, thanks!
+                  Cerrar Receta
                 </button>
               </div>
             </DialogPanel>
